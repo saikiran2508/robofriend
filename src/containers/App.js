dc6b651d01fae1258b8a5a8afa35e8cad0f5f866
Sai Kiran Gopu <gsaikiran502@gmail.com>
2021-07-04 10:04:54 +0530

    redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,53 @@
-import React,{ useState,useEffect } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import Cardlist from '../components/cardlist';
 import Searchbox from '../components/searchbox';
 import Scroll from '../components/scroll'
 import 'tachyons';
 import './App.css'
-import ErrorBoundry from '../components/ErrorBoundry';
+import { requestRobots, setSearchField } from '../actions.js';
 
-function App() {
-    
-    const [robots , setRobots] = useState([])
-    const [searchfield, setSearchfield] = useState('')
-    const [count,setCount] = useState(0)
-
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(responce=>responce.json())
-        .then(users=>{setRobots(users)});
-        console.log(count)
-    },[count])
-
-    const onserachchange = (event) => {
-        setSearchfield(event.target.value)
+const mapStateToProps = state =>{
+    return {
+        searchField:state.searchRobots.searchField,
+        robots:state.requestRobots.robots,
+        isPending:state.requestRobots.isPending,
+        error:state.requestRobots.error
     }
-    
-    const filteredrobots = robots.filter(robot=>{
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-
-    if(!robots.length){
-        return <h1 className='f2 tc'>LOADING</h1>;
-    }
-    else{
-        return(
-            <div className = 'tc'>
-                <h1 className='f1'>RoboFriend</h1>
-                <button onClick={()=>setCount(count+1)}>Click Me</button>
-                <Searchbox searchchange={onserachchange}/>
-                <Scroll>
-                    <ErrorBoundry>
-                        <Cardlist robots={filteredrobots}/>
-                    </ErrorBoundry>
-                </Scroll>
-            </div>
-        );
-    } 
 }
-export default App;
+
+const mapDispatchToProps = (dispatch) =>{
+    return{
+         onSearchChange:(event)=>dispatch(setSearchField(event.target.value)),
+         onRequestRobots:()=>requestRobots(dispatch)
+    }  
+}
+
+class App extends React.Component{
+
+    componentDidMount(){
+        this.props.onRequestRobots();
+    }
+    
+    render() {
+        const { searchField,onSearchChange,robots,isPending } = this.props;
+        const filteredrobots = robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        if(isPending){
+            return <h1 className='f2 tc'>LOADING</h1>;
+        }
+        else{
+            return(
+                <div className = 'tc'>
+                    <h1 className='f1'>RoboFriend</h1>
+                    <Searchbox searchchange={onSearchChange}/>
+                    <Scroll>
+                        <Cardlist robots={filteredrobots}/>
+                    </Scroll>
+                </div>
+            );
+        }
+    }   
+}
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore,applyMiddleware,combineReducers } from 'redux';
+import {createLogger} from 'redux-logger';
+import  thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App.js';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { searchRobots,requestRobots } from './reducers';
+
+const logger = createLogger();
+const rootReduser = combineReducers({searchRobots,requestRobots})
+const store = 
+  createStore(rootReduser , applyMiddleware(thunkMiddleware,logger))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
 
